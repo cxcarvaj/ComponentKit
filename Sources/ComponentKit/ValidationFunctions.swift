9868a49983396efed02caf32ceb8803b23cfebcf
cxcarvaj <cxcarvaj@gmail.com>
2025-04-14 08:34:25 -0500

    Adding isValidUsername into ValidationFunctions

--- Sources/ComponentKit/ValidationFunctions.swift
+++ Sources/ComponentKit/ValidationFunctions.swift
@@ -10,6 +10,7 @@ import Foundation
 public enum ValidationFunctions {
     case isNotEmpty
     case isEmail
+    case isValidUsername
 }
 
 extension ValidationFunctions {
@@ -19,11 +20,17 @@ extension ValidationFunctions {
             !value.isEmpty ? nil : " field is required."
         case .isEmail:
             isValidEmail(value) ? nil : " not a valid email."
+        case .isValidUsername:
+            validStringLength(value, min: 8, max: 32) ? nil : " must be between 8 and 32 characters."
         }
     }
     
-    func isValidEmail(_ email: String) -> Bool {
+    private func isValidEmail(_ email: String) -> Bool {
         let emailRegex = /^(?:[A-Z0-9a-z._%+-]+)@(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,63}$/
         return email.wholeMatch(of: emailRegex) != nil
+    }
+    
+    private func validStringLength(_ value: String, min: Int, max: Int) -> Bool {
+        (min...max).contains(value.count)
     }
 }
